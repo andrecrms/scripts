c928dccc6908024fa3a079d0a60489b707917cdd
André Rodrigues <44575552+andrecrms@users.noreply.github.com>
2025-07-30 21:31:56 -0400

    Update SQL Server Best Practices Assessment v21.ps1

--- SQL Server Best Practices Assessment v21.ps1
+++ SQL Server Best Practices Assessment v21.ps1
@@ -22,7 +22,7 @@ Write-Host @"
 # LinkedIn: https://www.linkedin.com/in/andre-c-rodrigues
 # Blog: http://sqlmagu.blogspot.com.br
 # GitHub: https://github.com/andrecrms
-# Last modified: 07/15/2025.
+# Last modified: 07/30/2025.
 =============================================================================================================================================================================================
 "@ -ForegroundColor Yellow
 Write-Host @"
