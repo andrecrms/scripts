ac61bd6c9747925e5a68763cfa1c0d44112cd857
André Rodrigues <44575552+andrecrms@users.noreply.github.com>
2026-03-23 09:38:30 -0400

    Update last modified date in PowerShell script

--- SQL Server Best Practices Assessment v21.ps1
+++ SQL Server Best Practices Assessment v21.ps1
@@ -23,7 +23,7 @@ Write-Host @"
 # LinkedIn: https://www.linkedin.com/in/andre-c-rodrigues
 # Blog: http://sqlmagu.blogspot.com.br
 # GitHub: https://github.com/andrecrms
-# Last modified: 03/23/2025.
+# Last modified: 03/23/2026.
 =============================================================================================================================================================================================
 "@ -ForegroundColor Yellow
 Write-Host @"
